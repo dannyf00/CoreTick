dfeed247a704f92029b66743fd820848570d1590
dannyf00 <fdan00@live.com>
2017-06-15 06:17:58 -0400

    Create main.c

--- main.c
+++ main.c
@@ -1,54 +1,34 @@
-//#include <lpc122x.h>				//we use lpc122x
+//#include <lpc122x.h>					//we use lpc122x
 #include "gpio.h"					//we use F_CPU + gpio functions
 #include "delay.h"					//we use software delays
-#include "coretick.h"				//we use dwt
+#include "coretick.h"					//we use dwt
 //#include "dhry.h"					//we use dhrystone benchmark
 
 //hardware configuration
 #define LED_PORT		GPIOC
-#define LED				(1<<8)
-#define LED_DLY			100			//counts
+#define LED			(1<<8)
+#define LED_DLY			100				//counts
 //end hardware configuration
 
 //global defines
 
 //global variables
-uint32_t tmp;
-
-//flip led
-void led_flp(void) {
-	//static int count=0;							//count
-	IO_FLP(LED_PORT, LED);					//flip the led
-}
-
-//set led
-void led_set(void) {
-	//static int count=0;							//count
-	IO_SET(LED_PORT, LED);					//flip the led
-}
-
-//clr led
-void led_clr(void) {
-	//static int count=0;							//count
-	IO_CLR(LED_PORT, LED);					//flip the led
-}
-
 
 int main(void) {
 	uint32_t time0, time1;
 
-	mcu_init();									//reset the  mcu
-	coretick_init();								//reset systick
+	mcu_init();							//reset the  mcu
+	coretick_init();						//reset systick
 	IO_OUT(LED_PORT, LED);						//led3/4 as output
-	tmp=F_CPU;									//read f_cpu - for debugging
+	tmp=F_CPU;							//read f_cpu - for debugging
 	ei();
 	while (1) {
-		IO_FLP(LED_PORT, LED);				//flip led
+		IO_FLP(LED_PORT, LED);					//flip led
 
-		time0=ticks();
-		//dhrystone();
-		coretick_delayms(LED_DLY);
-		time1=ticks() - time0;
-		if (time1>10) NOP();
+		time0=ticks();						//time stamp time0
+		//dhrystone();						//benchmark
+		coretick_delayms(LED_DLY);				//waste some time
+		time1=ticks() - time0;					//time stamp time1
+		if (time1>10) NOP();					//break point
 	};
 }
